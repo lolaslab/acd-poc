efa70cb5e70b109e5375b27a8057bfbcd68d1cab
Lola <lolaodelola@gmail.com>
2025-12-28 22:42:39 +0000

    Create compat files -wip

--- lib/constructor.rb
+++ lib/constructor.rb
@@ -5,31 +5,76 @@ require 'yajl'
 
 class Constructor
   include Enumerable
-  BROWSERS_PATH = File.expand_path(
-    '../data/util/browsers/',
+  CHROME_PATH = File.expand_path(
+    '../data/util/browsers/chrome.json',
     __dir__
   )
-  def init(directory=BROWSERS_PATH)
-    @directory = directory
-    Dir.glob(File.join(@directory, "*.json")).each do |file|
-      File.open(file, 'r') do |io|
-        parser = Yajl::Parser.new(symbolize_keys: true)
-        parser.on_parse_complete = lambda do |json|
-          if json["results"]
-            # binding.pry
-            json["results"].each do |subtest_obj|
-              element_file = File.expand_path("../data/html/elements/#{subtest_obj.first}.json", __dir__)
-              File.open(element_file, 'r') do |element_io|
-                element_data = Yajl::Parser.parse(element_io)
-                # binding.pry
-                # TODO: Update the element json file with the support data
-              end
-            end
+  JAWS_PATH = File.expand_path(
+    '../data/util/screen-readers/jaws.json',
+    __dir__
+  )
+  def self.init(browser = CHROME_PATH, screen_reader = JAWS_PATH)
+    # iterate over the screen reader object
+    screen_reader_io = File.open(screen_reader, 'r')
+    browser_obj = JSON.parse(File.read(browser))
+    browser_results = browser_obj["results"]
+    browser_version = browser_obj["version"]
+
+    parser = Yajl::Parser.new(symbolize_keys: true)
+    parser.on_parse_complete = lambda do |sr_element_obj|
+      sr_element_obj.each do |sr_data|
+        element = sr_data[:Element]
+        # find the element object per element in the browser object, skip if there are no tests for this element
+        next unless browser_results[element]
+        browser_element = browser_results[element]
+        # check the screen reader object "supported" key
+        #   if "yes" then the screen reader supports the object
+        if sr_data[:Support] == "[yes]"
+          sr_support = "1"
+        elsif sr_data[:Support] == "[no]"
+          sr_support = "0"
+        else
+          sr_support = "2"
+          notes = sr_data[:Support]
+        end
+        # check the stats object in the browser object
+        browser_element_stats = browser_element[-1]["stats"]
+        #   if the total == the passes then the browser supports the object
+        if browser_element_stats["passes"] == browser_element_stats["total_subtests"]
+          b_support = browser_version
+        else
+          b_support = 0
+        end
+        # find the element file per element in the screen reader object
+        element_acd_filename = "data/html/elements/#{element}.json"
+        if File.zero?(element_acd_filename)
+          puts "Skipping empty file for: #{element}"
+          element_acd_obj = {}
+        else
+          element_acd_obj = JSON.parse(File.read(element_acd_filename))
+          compat_node = element_acd_obj.dig("html", "elements", element, "__compat")
+          compat_node["support"] = {
+            "chrome/jaws" => "#{b_support}/#{sr_support}"
+          }
+          if notes
+            compat_node["notes"] = notes
           end
+          File.write(element_acd_filename, JSON.pretty_generate(element_acd_obj))
         end
       end
     end
+
+
+    #   update the element file with
+    #     "support": {
+    #         "chrome/jaws": {
+    #           "version_added: "brow_v/sr_v",
+    #
+    #         }
+    #      }
+
+    parser.parse(screen_reader_io)
   end
 end
 
-puts Constructor.new.init
+puts Constructor.init
